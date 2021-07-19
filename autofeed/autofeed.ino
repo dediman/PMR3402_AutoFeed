5391c91e1a4fb90b6b1b0a45edfb9d6c8a7813ab
Luca Tamashiro Decker <kenzotamashiro@usp.br>
2021-07-19 02:00:25 -0300

    criar máquina de estados

--- autofeed/autofeed.ino
+++ autofeed/autofeed.ino
@@ -5,12 +5,65 @@
 #include "button.h"
 #include "lcd.h"
 #include "led.h"
+#include "servomotor.h"
+#include "ultrassonic.h"
 
-// instanciamento de classes
+// Instanciamento de Classes
 Buzzer buz;
 Button btn;
 Lcd lcd;
 Led led;
+Servomotor ser;
+Ultrassonic ult;
+
+// Variaveis Globais
+int acao_matrizTransicaoEstados[NUM_ESTADOS][NUM_EVENTOS];
+int proximo_estado_matrizTransicaoEstados[NUM_ESTADOS][NUM_EVENTOS];
+
+// Maquina de Estados
+void iniciaMaquinaEstados()
+{
+  for (int i = 0; i < NUM_ESTADOS; i++) {
+    for (int j = 0; j < NUM_EVENTOS; j++) {
+       acao_matrizTransicaoEstados[i][j] = NENHUMA_ACAO;
+       proximo_estado_matrizTransicaoEstados[i][j] = i;
+    }
+  }
+  proximo_estado_matrizTransicaoEstados[DEFINIR_TEMPO][INCREMENTAR] = DEFINIR_TEMPO;
+  acao_matrizTransicaoEstados[DEFINIR_TEMPO][INCREMENTAR] = A01;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_TEMPO][DECREMENTAR] = DEFINIR_TEMPO;
+  acao_matrizTransicaoEstados[DEFINIR_TEMPO][DECREMENTAR] = A02;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_TEMPO][CONFIRMAR] = DEFINIR_PORCOES;
+  acao_matrizTransicaoEstados[DEFINIR_TEMPO][CONFIRMAR] = A04;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_PORCOES][INCREMENTAR] = DEFINIR_PORCOES;
+  acao_matrizTransicaoEstados[DEFINIR_PORCOES][INCREMENTAR] = A01;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_PORCOES][DECREMENTAR] = DEFINIR_PORCOES;
+  acao_matrizTransicaoEstados[DEFINIR_PORCOES][DECREMENTAR] = A02;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_PORCOES][REDEFINIR] = DEFINIR_TEMPO;
+  acao_matrizTransicaoEstados[DEFINIR_PORCOES][REDEFINIR] = A03;
+
+  proximo_estado_matrizTransicaoEstados[DEFINIR_PORCOES][CONFIRMAR] = EM_OPERACAO;
+  acao_matrizTransicaoEstados[DEFINIR_PORCOES][CONFIRMAR] = A03;
+
+  proximo_estado_matrizTransicaoEstados[EM_OPERACAO][REDEFINIR] = DEFINIR_TEMPO;
+  acao_matrizTransicaoEstados[EM_OPERACAO][REDEFINIR] = A03;
+
+  proximo_estado_matrizTransicaoEstados[EM_OPERACAO][TEMPO_ATINGIDO] = LIBERANDO_RACAO;
+  acao_matrizTransicaoEstados[EM_OPERACAO][TEMPO_ATINGIDO] = A05;
+
+  proximo_estado_matrizTransicaoEstados[EM_OPERACAO][TEMPO_NAO_ATINGIDO] = EM_OPERACAO;
+  acao_matrizTransicaoEstados[EM_OPERACAO][TEMPO_NAO_ATINGIDO] = A06;
+
+  proximo_estado_matrizTransicaoEstados[LIBERANDO_RACAO][RACAO_LIBERADA] = EM_OPERACAO;
+  acao_matrizTransicaoEstados[LIBERANDO_RACAO][RACAO_LIBERADA] = A07;
+
+} // iniciaMaquinaEstados
+
 
 void setup() {
   
--- autofeed/definitions.h
+++ autofeed/definitions.h
@@ -1,11 +1,39 @@
 #ifndef DEFINITIONS_H_INCLUDED
 #define DEFINITIONS_H_INCLUDED
 
-// General
+// GENERAL
 #define HIGH 1
-#define LOW 0
+#define LOW 0`
 
-// Buzzer
+// MAQUINA DE ESTADOS
+#define NUM_ESTADOS 4
+#define NUM_EVENTOS 7
+// Estados
+#define DEFINIR_TEMPO       0
+#define DEFINIR_PORCOES     1
+#define EM_OPERACAO         2
+#define LIBERANDO_RACAO     3
+// Eventos
+#define NENHUM_EVENTO      -1
+#define INCREMENTAR         0
+#define DECREMENTAR         1
+#define CONFIRMAR           2
+#define REDEFINIR           3
+#define TEMPO_ATINGIDO      3
+#define TEMPO_NAO_ATINGIDO  3
+#define RACAO_LIBERADA      3
+// Acoes
+#define NENHUMA_ACAO -1
+#define A01  0
+#define A02  1
+#define A03  2
+#define A04  3
+#define A05  4
+#define A06  5
+#define A07  6
+
+
+// BUZZER
 #define BUZZER_PIN 14
 #define TURN_ON 0
 #define CONFIRMED 1
@@ -22,21 +50,21 @@
 #define D5 = A3
 #define D6 = A4
 #define D7 = A5
-
+// Messages
 #define CHEIO "Cheio"
 #define VAZIO "Vazio"
 
-// Buttons
+// BUTTONS
 #define PLUS_PIN 7
 #define MINUS_PIN 8
 #define ENTER_PIN 9
 #define REDEFINE_PIN 10
 
-// Ultrassonic
+// ULTRASSONIC
 #define TRIG_PIN 11
 #define ECHO_PIN 12
 
-// Servo
+// SERVO
 #define SIGNAL_PIN 9
 
 #endif // DEFINITIONS_H_INCLUDED
