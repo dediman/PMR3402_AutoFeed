002805040e1c7d3bde8405c08ac3c3c8e6bb7d4c
Luca Tamashiro Decker <kenzotamashiro@usp.br>
2021-07-21 19:06:07 -0300

    ajustar velocidade do servo

--- autofeed/servomotor.cpp
+++ autofeed/servomotor.cpp
@@ -36,17 +36,15 @@ void Servomotor::resetCounter()
 
 void Servomotor::activate()
 {
-  Serial.println("## SERVO ATIVADO ##");
   if(feedCounter > 0) {
-    for (position = 0; position <= 250; position++) { // de 0 a 180 graus
-      myservo.write(position);              // movimenta servo ate a posicao
-      delay(30);   // aguarda 15ms para o servo atingir a posicao
+    for (position = 0; position <= 250; position++) {
+      myservo.write(position);  // movimenta servo ate a posicao
+      delay(15);   // aguarda 15ms para o servo atingir a posicao
     }
-    for (position = 250; position >= 0; position--) { // de 180 a 0 graus
-      myservo.write(position);              // movimenta servo ate a posicao
-      delay(30);   // aguarda 15ms para o servo atingir a posicao
+    for (position = 250; position >= 0; position--) {
+      myservo.write(position);  // movimenta servo ate a posicao
+      delay(15);   // aguarda 15ms para o servo atingir a posicao
     }
     decrementCounter();
   }
-  
 }
